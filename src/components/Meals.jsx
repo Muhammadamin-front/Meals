e5565305c926863169105bc4738b0176d03edef1
berdullayev@gmail.com <macbookpro@MACBOOKnoMacBook-Pro.local>
2026-06-10 23:30:02 +0500

    main

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,22 +2,21 @@ import axios from "axios";
 import "../index.css"
 import { useEffect, useState } from "react";
 const Meals = () => {
+    const API = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
 const [items,setItems] = useState([])
 useEffect(()=>{
-    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    axios.get(API)
     .then((res) =>{
-        console.log(items);
-        
         setItems(res.data.meals)
     })
     .catch(error =>{
         return error
     })
 },[])
-const itemslist = items.map(({strMeal,strMealThumd,idMeal})=>{
+const itemslist = items.map(({strMeal,strMealThumb,idMeal})=>{
     return (
         <section key={idMeal} className="card">
-            <img src={strMealThumd} alt="Image" />
+            <img src={strMealThumb} alt={strMeal} />
             <section className="content">
                 <p>{strMeal}</p>
                 <p>#{idMeal}</p>
@@ -26,7 +25,7 @@ const itemslist = items.map(({strMeal,strMealThumd,idMeal})=>{
     )
 })
 
-  return <div>
+  return <div className="cards">
 {itemslist}
   </div>;
 };
